a6198ce2937f71c093615c85425173a980dc6107
javier-local <rivadeneirajavier7@gmail.com>
2022-10-05 11:35:25 -0300

    f

---
+++ alto-codigo/Reto6.py
@@ -0,0 +1,3 @@
+#Generar un número aleatorio entre 1 y 120. Decir si se encuentra en el intervalo entre 10 y 50, 
+# o bien si es mayor de 50 hasta 100, 
+# o bien si es mayor de 100, o bien si es menor de 10.
